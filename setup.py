54a4c7fcff0afa6d7953058168f1b28f99ce68b7
Tarek39 <itcameranian@gmail.com>
2021-03-17 00:03:23 +0900

    [delete]名前を変更した。 中身が完全にサンプルだったから。

--- setup.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-driver_dir = "./chrome/chromedriver.exe"
-driver = webdriver.Chrome(driver_dir)
-driver.get('https://google.com/')
