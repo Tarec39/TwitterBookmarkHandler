fe667f6232b24ca199f8f88ac6a2bf428deacfc2
Tarek39 <itcameranian@gmail.com>
2021-03-18 01:12:14 +0900

    [update] scroll_down_2で行けそう。 最下部に到達したらbreakしたいがよくわからなくなってきたので、次からはコマンド入力とか物理的な方向で解決していこうと思います。

--- sample.py
+++ sample.py
@@ -8,7 +8,7 @@ import time
 driver = webdriver.Chrome(os.environ['DRIVER_DIR'])
 driver.get('https://twitter.com/login')
 
-time.sleep(5)
+time.sleep(2)
 
 input_id = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[1]/label/div/div[2]/div/input')
 input_pass = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[2]/label/div/div[2]/div/input')
@@ -31,7 +31,7 @@ time.sleep(1)
 
 btn_login.click()
 
-time.sleep(5)
+time.sleep(2)
 
 bookmark = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/header/div/div/div/div[1]/div[2]/nav/a[5]/div')
 
@@ -40,27 +40,26 @@ time.sleep(3)
 
 def scroll_down():
   	#ページの高さを取得
-    while True:
         height = driver.execute_script("return document.body.scrollHeight")
-        for x in range(1,height):
-            driver.execute_script("window.scrollTo(0, "+str(x*2)+");")
-            height = driver.execute_script("return document.body.scrollHeight")
-
-
+    # while True:
+    #     for x in range(1,height):
+    #         driver.execute_script("window.scrollTo(0, "+str(x*2)+");")
+    #         height = driver.execute_script("return document.body.scrollHeight")
+    #     break
 
 def scroll_down_2():
     SCROLL_PAUSE_TIME = 2
     last_height = driver.execute_script("return document.body.scrollHeight")
+    x = 0
     while True:
         # Scroll down to bottom
-        driver.execute_script("window.scrollTo(0, "+str(x*2)+");")
-
-        # Calculate new scroll height and compare with last scroll height
+        x = x + 1
+        driver.execute_script("window.scrollTo(0, "+str(x*4)+");")
         new_height = driver.execute_script("return document.body.scrollHeight")
-
-        # break condition
         if new_height == last_height:
+            print('Reached')
             break
+    
 def scroll_down_3():
     i = 1
     height = driver.excute_script("return document.body.scrollHeight")
@@ -70,7 +69,7 @@ def scroll_down_3():
             i = i*2
         height = driver.excute_script("return document.body.scrollHeight")
 
-scroll_down_3()
+scroll_down_2()
 
 time.sleep(3)
 driver.quit()
